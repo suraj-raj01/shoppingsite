b4d2794009786c2073508ad01da79d5b8320b35d
SURAJ <surajk38656@gmail.com>
2026-02-21 17:16:06 +0530

    shopping site updated

--- client-ui/src/Config.tsx
+++ client-ui/src/Config.tsx
@@ -1,5 +1,5 @@
 
-// const BASE_URL="https://fullstack-ecommerce-ln2w.onrender.com";
-const BASE_URL="http://localhost:8000";
+const BASE_URL="https://fullstack-ecommerce-ln2w.onrender.com";
+// const BASE_URL="http://localhost:8000";
 
 export default BASE_URL;
