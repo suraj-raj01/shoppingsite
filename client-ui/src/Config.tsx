f9ebbc4d30d3b03248df3d06b6074e8043af27b9
SURAJ KUMAR <surajk38656@gmail.com>
2026-02-23 17:41:03 +0530

    Switch BASE_URL to production endpoint

--- client-ui/src/Config.tsx
+++ client-ui/src/Config.tsx
@@ -1,5 +1,4 @@
-
-// const BASE_URL="https://shoppingsite-znka.onrender.com";
-const BASE_URL="http://localhost:8000";
+const BASE_URL="https://shoppingsite-znka.onrender.com";
+// const BASE_URL="http://localhost:8000";
 
 export default BASE_URL;
